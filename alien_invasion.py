02dd132a1b57519b0ca57fa7926e91d0dc3c3da5
Michael Giardina <mgiardi0@bu.edu>
2018-04-28 14:01:10 -0400

    Restore from git error

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,24 +1,39 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
+
 from ship import Ship
 import game_functions as gf
 
 def run_game():
-    # Initialize game and create a screen objectself.
+    # Initialize pygame, settings, and screen object.
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # Make a ship.
-    ship = Ship(screen)
 
-    # Start the main loop for the gameself.
+
+    # Set the background color.
+    bg_color = (255, 255, 255)
+
+    # Make a ship, a group of bullets, and a group of aliens.
+    ship = Ship(screen)
+    bullets = Group()
+    aliens = Group()
+
+    # Create the fleet of aliens.
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
+
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+        gf.update_aliens(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
--- settings.py
+++ settings.py
@@ -1,12 +1,26 @@
 class Settings():
-    """A class to store all settings for game."""
+    """A class to store all settings for Alien Invasion."""
 
     def __init__(self):
-        """Initialize the settings."""
-        # Screen settings
+        """Initialize the game's settings."""
+        # Screen settings.
         self.screen_width = 1600
         self.screen_height = 900
         self.bg_color = (255, 255, 255)
 
-        # Ship Settings
-        self.ship_speed_factor = 1.5
+        # Ship settings.
+        self.ship_speed_factor = 1.9
+        self.ship_limit = 3
+
+        # Bullet settings.
+        self.bullet_speed_factor = 3
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
+
+        # Alien settings.
+        self.alien_speed_factor = 1.5
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right, -1 represents left.
+        self.fleet_direction = 1
